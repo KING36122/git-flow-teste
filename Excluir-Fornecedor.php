80a84a3151ddd2cc8ec20f56f816636acf2f688b
36122KING <121176262+36122KING@users.noreply.github.com>
2022-12-21 10:09:11 -0400

    📦 [PHP] Criação do Excluir Fornecedor

---
+++ Excluir-Fornecedor.php
@@ -0,0 +1,11 @@
+<?php
+	include "cabecalho.php";
+	include_once "conn/conexao.php";
+	include "funcoes.php";
+	
+		$idFornecedor = $_GET["IdFornecedor"];
+		$fornecedor = excluirFornecedor($conexao, $idFornecedor);
+		
+		echo "<meta http-equiv='refresh' content='0;Listar-Fornecedor.php?'></meta>";
+	include "rodape.php";
+?>
